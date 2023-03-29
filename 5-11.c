20b0f7924640aa2020aeb77a4952c74dd659af77
qkralsrud <127359599+qkralsrud@users.noreply.github.com>
2023-03-29 19:48:56 +0900

    Add files via upload

---
+++ 5-11.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void main()
+{
+	int a, b;
+	char c;
+
+	printf("수식을 한줄로 띄어쓰기로 입력하세요 : ");
+	scanf("%d %c %d", &a, &c, &b);
+
+	switch(c)
+	{
+	    case '+': printf("%d + %d = %d", a, b, a+b); break;
+	    case '-': printf("%d - %d = %d", a, b, a-b); break;
+	    case '*': printf("%d * %d = %d", a, b, a*b); break;
+	    case '/': printf("%d / %d = %d", a, b, a/b); break;
+	    case '%': printf("%d % %d = %d", a, b, a%b); break;
+            default :printf("연산자를 잘못 입력했습니다.");
+	}
+}
